75454cbba652c5dc2c326a3b1fc99a56dd4009f4
Carles Sala <c.sala.stq@gmail.com>
2018-07-13 12:01:20 -0400

    Upgrade versions

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools import find_packages, setup
 
 
 install_requires = [
-    'd3m==v2018.7.10',
-    'mlblocks>=0.1.6',
+    'd3m==2018.7.10',
+    'mlblocks==0.1.7',
 ]
 
 tests_require = [
@@ -20,16 +20,11 @@ setup_requires = [
 development_requires = [
     'autoflake>=1.1',
     'autopep8>=1.3.5',
-    # 'bumpversion>=0.5.3',
-    # 'coverage>=4.5.1',
     'flake8>=3.5.0',
     'isort>=4.3.4',
     'pip>=10.0.1',
     'pycodestyle==2.3.1',
     'pyflakes==1.6.0',
-    # 'tox>=2.9.1',
-    # 'twine>=1.10.0',
-    # 'wheel>=0.30.0',
 ]
 
 extras_require = {
@@ -56,8 +51,6 @@ setup(
     name='mit-primitives',
     packages=find_packages(include=['mit_primitives', 'mit_primitives.*']),
     setup_requires=setup_requires,
-    # test_suite='tests',
-    # tests_require=tests_require,
     version='0.0.1-dev',
     zip_safe=False,
 )
